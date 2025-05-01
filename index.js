f276eb98b0e19b626ae64778c13562514292320b
Isabella <Isabella.emerichs@gmail.com>
2025-05-01 16:44:16 -0300

    faz a primeira configuração de rotas e métodos

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const path = require("path");
+const routes = require ("./routes/routes")
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
-//ROTA
-app.get("/home", (req, res) => {
-  res.render("index");
-});
 app.use(express.static(path.join(__dirname, "public")));
+app.use (routes);
+
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
